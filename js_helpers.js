6a92d01936e73a6340e6895a4d1026c678890edf
votslot <mpolotskaia64@gmail.com>
2025-01-08 11:31:13 -0800

    182025

--- js_helpers.js
+++ js_helpers.js
@@ -29,6 +29,23 @@ function OnTest( v) {
     test_cb = Module.cwrap('OnTestJS', 'number', ['number']);
     test_cb(v);
 }
+/*
+function loadFile(filePath) {
+    const req = new XMLHttpRequest();
+    req.open("GET", filePath, true);
+    req.responseType = "arraybuffer";
+    req.onload = (event) => {
+        const arrayBuffer = req.response;
+        if (arrayBuffer) {
+            console.log("A AM HERE" + arrayBuffer.byteLength);
+            const byteArray = new Uint8Array(arrayBuffer); 
+            byteArray.forEach((element, index) => {});
+            ReadBin(arrayBuffer, 0);
+        }
+    };
+    req.send(null);
+}
+*/
 
 function OnMouseMove(e) {
     gv.mouseX = e.offsetX;
@@ -138,6 +155,10 @@ function OnUIEvent1(input){
     if( input.id == "ruler"){
         setRuler(document.getElementById(input.id).checked); 
     }
+    if( input.id == "SampleLasId"){
+        console.log("las id");
+        loadFile("./data/sample.las");
+    }
     // gray out some UI elements
     if( input.id == "camOrto"){
         fovEl = document.getElementById("fovVal");
@@ -147,6 +168,7 @@ function OnUIEvent1(input){
             fovEl.removeAttribute('disabled');
         }
     }
+    /*
     if( input.id == "rdAll"){
         budgetEl = document.getElementById("budVal");
         if(document.getElementById(input.id).checked==true){
@@ -155,6 +177,7 @@ function OnUIEvent1(input){
             budgetEl.removeAttribute('disabled');
         }
     }
+    */
 }
 
 function OnBkhanged(input){
